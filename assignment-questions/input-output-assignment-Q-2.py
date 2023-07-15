a46a178d5e1ca44b63428796169a18cc9a5c2b55
Anilkumar Para <50998167+AnilkumarPara@users.noreply.github.com>
2023-07-15 18:13:37 +0530

    input and output functions assignment questions

---
+++ assignment-questions/input-output-assignment-Q-2.py
@@ -0,0 +1,6 @@
+'''
+Write ‘Welcome to Anil ITEducation Channel’ text
+to output.txt file using the print function
+'''
+
+
---
+++ assignment-questions/input-output-assignment-Q-3.py
@@ -0,0 +1,5 @@
+'''
+WAP to take 2 inputs from the user inside the print function,
+print those inputs using the print function
+'''
+
