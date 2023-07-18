835a08b1f66f6a3f992a25f45c6740f0fb1f9b26
Anilkumar Para <50998167+AnilkumarPara@users.noreply.github.com>
2023-07-18 15:19:07 +0530

    minor change updated

--- assignment-questions/P13-variables-assignment-Q-1.py
+++ assignment-questions/P13-variables-assignment-Q-1.py
@@ -1,5 +1,5 @@
 '''
 WAP to assign 3 values (string, int, float)
-to 3 variables (Student_name, Rool_Number, Percentage_of_marks)
-print the values using print function
+to 3 variables (Student_name, Rool_Number, Percentage_of_marks).
+print the values using print function.
 '''
