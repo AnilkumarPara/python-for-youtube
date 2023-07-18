ba7797d8fc0b42ad17ecd14dc44be25f6959990d
Anilkumar Para <50998167+AnilkumarPara@users.noreply.github.com>
2023-07-18 16:00:33 +0530

    string assignment solutions

---
+++ assignment-answers/P21-strings-assignment-QA-1.py
@@ -0,0 +1,7 @@
+'''
+Write a Program to retrieve character 'X' using positive indexing
+from "Semantic segmentation of Lung from Chest X-Ray" string
+'''
+
+s1 = "Semantic segmentation of Lung from Chest X-Ray"
+print(s1[41])
