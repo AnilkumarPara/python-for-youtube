29ebe7193afa27ad1aa85f2c726ccef97d47c7d0
Anilkumar Para <50998167+AnilkumarPara@users.noreply.github.com>
2023-07-18 15:16:43 +0530

    Minor changes in the questions

--- assignment-answers/P14-variables-assignment-QA-2.py
+++ assignment-answers/P14-variables-assignment-QA-2.py
@@ -1,7 +1,6 @@
 '''
-WAP to assign value 6 to two variables (example: a1, b1),
-check the variable values and address
-Write variable creation in single line of code
+WAP to assign value 6 to two variables (example: a1, b1) using single line of code.
+check the variable values and address.
 '''
 a1, b1 = 6, 6
 print(a1, b1)
