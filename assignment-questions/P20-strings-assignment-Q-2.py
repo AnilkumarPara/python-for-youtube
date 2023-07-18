39e7e4eb2b84f7960714757a09ec51d8f9a362c1
Anilkumar Para <50998167+AnilkumarPara@users.noreply.github.com>
2023-07-18 15:59:07 +0530

    Delete P20-strings-assignment-Q-2.py

--- assignment-questions/P20-strings-assignment-Q-2.py
+++
@@ -1,8 +0,0 @@
-'''
-Write a Program to retrieve the 'L' character from "I Love Python Programming" string
-using negative/reverse indexing
-'''
-
-s2 = "I Love Python Programming"
-print(s2[-23])
-
