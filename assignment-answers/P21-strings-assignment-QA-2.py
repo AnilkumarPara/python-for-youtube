ecac32dc9d6f017a3b7820b94f718f1143a2b500
Anilkumar Para <50998167+AnilkumarPara@users.noreply.github.com>
2023-07-18 15:43:34 +0530

    string assignment solutions

---
+++ assignment-answers/P21-strings-assignment-QA-2.py
@@ -0,0 +1,8 @@
+'''
+Write a Program to retrieve the 'L' character from "I Love Python Programming" string
+using negative/reverse indexing
+'''
+
+s2 = "I Love Python Programming"
+print(s2[-23])
+
