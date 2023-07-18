231d306c4e2cc6729086236bc0970b9f8e0d1445
Anilkumar Para <50998167+AnilkumarPara@users.noreply.github.com>
2023-07-18 16:32:24 +0530

    list and nested list assignment solutions

---
+++ assignment-answers/P23-list-nested-list-assignment-QA-6.py
@@ -0,0 +1,5 @@
+# WAP to retrieve the 'Sweet' string from the following nested list using Positive indexing
+
+L6 = [21, ['Anil', 'Education', [['Java', 'Kova'], ['Programming', 'Sweet']]], 7065, 5, 2034, [1, 2]]
+
+print(L6[1][2][1][1])
