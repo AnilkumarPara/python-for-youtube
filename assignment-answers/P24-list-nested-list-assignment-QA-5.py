3e0954272f2d08690648673c9d780fd7a79f54dd
Anilkumar Para <50998167+AnilkumarPara@users.noreply.github.com>
2023-07-18 17:18:32 +0530

    list & nested list assignment solutions

---
+++ assignment-answers/P24-list-nested-list-assignment-QA-5.py
@@ -0,0 +1,5 @@
+# WAP to retrieve the character 'u' from the following nested list using negative indexing
+
+L5 = [21, ['Anil', 'Education', ['Java', 'Kova']], 7065, 2001, 5, 2034, [1, 2]]
+
+print(L5[-6][-2][-7])
