495f9f89f846720863b8f8b6baf09b21d10a8179
Anilkumar Para <50998167+AnilkumarPara@users.noreply.github.com>
2023-09-12 12:24:48 +0530

    for loop class programs

---
+++ class-programs/control-statements/loops/P70-fruits-for-loop.py
@@ -0,0 +1,12 @@
+# WAP to access each fruit from the fruits container
+fruits = ["apple", "banana", "orange"]
+for fruit in fruits:
+    print(fruit)
+print("End of the program")
+
+
+
+
+
+
+
---
+++ class-programs/control-statements/loops/P70-sum-numbers-list-for.py
@@ -0,0 +1,12 @@
+# WAP to find the sum of the numbers
+numbers = [1, 2, 3, 4, 5]
+sum_value = 0
+for number in numbers:
+    sum_value = sum_value + number
+print("sum of the values = ", sum_value)
+
+
+
+
+
+
