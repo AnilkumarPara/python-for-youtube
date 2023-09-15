578a962995af5027d3d2b01cc9e7c07203d6e2b9
Anilkumar Para <50998167+AnilkumarPara@users.noreply.github.com>
2023-09-15 19:29:24 +0530

    pass and return statements class programs

---
+++ class-programs/control-statements/unconditional-branching/P73-pass-realtime-example.py
@@ -0,0 +1,18 @@
+def account_management():
+    # Code to be implemented later
+    pass
+
+
+def marketdata_analysis():
+    # Code to be implemented later
+    pass
+
+
+account_management()
+marketdata_analysis()
+
+while True:
+    # Code to be implemented later
+    pass
+
+
---
+++ class-programs/control-statements/unconditional-branching/P73-return-add-func.py
@@ -0,0 +1,7 @@
+def add_numbers(a, b):
+    result = a + b
+    return result
+
+
+sum_result = add_numbers(5, 3)
+print(sum_result)  # This will print "8"
