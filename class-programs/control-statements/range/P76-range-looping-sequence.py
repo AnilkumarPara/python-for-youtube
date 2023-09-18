121e21f5843925786e73b5d93c8e728f6f342ccd
Anilkumar Para <50998167+AnilkumarPara@users.noreply.github.com>
2023-09-18 17:57:45 +0530

    Add files via upload

---
+++ class-programs/control-statements/range/P76-range-looping-sequence.py
@@ -0,0 +1,9 @@
+# The primary use of range() is in for loops to iterate over a sequence of numbers
+# Looping Through a Sequence
+# WAP to print numbers from 1 to 10 using for loop
+for number in range(1, 11, 1):
+    print(number)
+
+
+
+
