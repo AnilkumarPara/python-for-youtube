50d70b00013d21eba0d2451a09baef47f0ad4879
Anilkumar Para <50998167+AnilkumarPara@users.noreply.github.com>
2023-09-18 17:57:13 +0530

    Add files via upload

---
+++ class-programs/control-statements/P76-generating-countdown.py
@@ -0,0 +1,10 @@
+# Generating a Countdown
+# Countdowns are often used in various applications
+# WAP to generate a countdown timer
+print("countdown begins")
+for count in range(10, 0, -1):
+    print(count)
+print("countdown ends")
+
+
+
