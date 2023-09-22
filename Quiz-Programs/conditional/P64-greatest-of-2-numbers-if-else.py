8df76efaf67373ed0ef0a11c913da3469e75c7c6
Anilkumar Para <50998167+AnilkumarPara@users.noreply.github.com>
2023-09-22 09:50:26 +0530

    Delete Quiz-Programs/conditional directory

--- Quiz-Programs/conditional/P64-greatest-of-2-numbers-if-else.py
+++
@@ -1,9 +0,0 @@
-# WAP to find the greatest of 2 numbers using if else condition
-a = int(input("Enter the value of 'a' "))
-b = int(input("Enter the value of 'b' "))
-if a > b:
-    print("a is greatest")
-else:
-    print("b is greatest")
-print("End of the program")
-
--- Quiz-Programs/conditional/P64-zero-number-if.py
+++
@@ -1,7 +0,0 @@
-# WAP to print ‘Zero’ if given number is 0 using the if condition
-n = int(input("Enter a number"))
-if n == 0:
-    print("Given number is Zero")
-print("End program")
-
-
