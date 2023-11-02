d22fd939a389f8a73bea4499675f9a56339a9448
Anilkumar Para <50998167+AnilkumarPara@users.noreply.github.com>
2023-11-02 08:16:05 +0530

    dict access key, balue

---
+++ class-programs/functions/iterables-built-in-functions/P118_for_loop_dict.py
@@ -0,0 +1,11 @@
+stock_prices = {
+    "AAPL": 150.62,
+    "GOOGL": 2700.45,
+    "MSFT": 299.47,
+    "AMZN": 3400.12,
+    "TSLA": 780.98
+}
+for key, val in stock_prices.items():
+    print(key, val)
+
+
