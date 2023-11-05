afd62586f52a248eb75abe909b399353af1e0eb1
Anilkumar Para <50998167+AnilkumarPara@users.noreply.github.com>
2023-11-05 22:20:54 +0530

    dictionary Comprehesion assignment

---
+++ assignment-questions/P122_fruits_frequency_dict_comp_Q_2.py
@@ -0,0 +1,7 @@
+"""
+Count the frequency of each fruit in a list and output as
+key: value
+fruit:count
+Write program using dictionary comprehension
+"""
+fruits = ["apple", "banana", "apple", "cherry", "banana"]
---
+++ assignment-questions/P122_string_lengths_dict_comp_Q_1.py
@@ -0,0 +1,8 @@
+"""
+create dictionary with string of lengths and output as
+key: value
+word:length
+Write program using dictionary comprehension
+"""
+words = ["hey", "Hello", "Covid"]
+
