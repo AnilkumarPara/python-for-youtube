866e999e605c7ae0ed4b5975b09b2c0667447cd0
Anilkumar Para <50998167+AnilkumarPara@users.noreply.github.com>
2023-11-05 15:12:05 +0530

    Assignment questions on list Comprehesion

---
+++ assignment-questions/P121_extract_words_list_comp_Q_2.py
@@ -0,0 +1,9 @@
+"""
+WAP to extract each word from a sentence and store all the words
+in the form of list using list comprehension
+"""
+
+paragraph = ["There was a fox.", 'It was brown in color.', "It was seen near that farm sometime back"]
+
+# Output
+['There', 'was', 'a', 'fox.', 'It', 'was', 'brown', 'in', 'color.', 'It', 'was', 'seen', 'near', 'that', 'farm', 'sometime', 'back']
---
+++ assignment-questions/P121_names_starts_ends_with_a_list_comp_Q_1.py
@@ -0,0 +1,8 @@
+"""
+WAP to find names starts with ‘A’ and ends with 'a'
+in the given list of names using list comprehension
+"""
+names = ['Abeesha', 'Abelia', 'Anil', 'Shanvika', 'Jayansh']
+
+
+
