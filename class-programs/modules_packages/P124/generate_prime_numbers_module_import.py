59e23db46da38ae2251bb7d4d01da3d3f9bed517
Anilkumar Para <50998167+AnilkumarPara@users.noreply.github.com>
2023-11-10 13:10:47 +0530

    diffferent types of imports

---
+++ class-programs/modules_packages/P124/generate_prime_numbers_module_import.py
@@ -0,0 +1,10 @@
+# Find all prime numbers between 2 and n
+import number_theory_functions
+
+n = int(input("Enter a 'n' value to find the prime numbers from '2 to n' "))
+if n > 1:
+    for number in range(2, n+1):
+        if number_theory_functions.prime_number(number):
+            print(number)
+else:
+    print("Prime numbers apply only to natural numbers greater than 1.")
