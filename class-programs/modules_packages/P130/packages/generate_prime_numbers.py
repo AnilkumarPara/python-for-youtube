1201ccaa71581f9d53bcc3e331332c4eb8a17731
Anilkumar Para <50998167+AnilkumarPara@users.noreply.github.com>
2023-11-24 14:23:04 +0530

    python packages

---
+++ class-programs/modules_packages/P130/packages/generate_prime_numbers.py
@@ -0,0 +1,6 @@
+# import functions.prime_tables_factorial_functions
+from functions import prime_tables_factorial_functions as ptf
+import sys
+print(sys.path)
+n = int(input("Enter n value "))
+ptf.generate_prime_numbers(5)
