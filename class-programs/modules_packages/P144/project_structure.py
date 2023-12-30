0f6370eddfe38e460e654747ff5d90eeaec28a34
Anilkumar Para <50998167+AnilkumarPara@users.noreply.github.com>
2023-12-30 21:56:56 +0530

    Python real-time project setup

---
+++ class-programs/modules_packages/P144/project_structure.py
@@ -0,0 +1,11 @@
+my_package/
+├── my_package/
+    ├── __init__.py
+    ├── module1.py
+    ├── module2.py
+├── tests/
+    ├── test_module1.py
+    ├── test_module2.py
+├── setup.py
+├── README.md
+├── LICENSE
