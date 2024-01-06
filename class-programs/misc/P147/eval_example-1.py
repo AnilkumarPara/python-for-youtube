cd06f928339070a4596af2b7ece8fd702dfd9dbb
Anilkumar Para <50998167+AnilkumarPara@users.noreply.github.com>
2024-01-06 18:58:34 +0530

    eval() function video related code

---
+++ class-programs/misc/P147/eval_example-1.py
@@ -0,0 +1,6 @@
+# suppose you get an expression in string format from different app
+# You want to evaluate as an expression
+x = 7
+expression = 'x+1'
+print(eval(expression))
+print(type(eval(expression)))
---
+++ class-programs/misc/P147/eval_global_local.py
@@ -0,0 +1,2 @@
+expression = "x + y"
+print(eval(expression, {'x': 9}, {'y': 20}))
