2b11f3c98199bf75c68b94626d73f3aa561e4e14
Anilkumar Para <50998167+AnilkumarPara@users.noreply.github.com>
2024-01-13 19:32:49 +0530

    main function code

---
+++ class-programs/misc/P-150/first_program.py
@@ -0,0 +1,13 @@
+def main():
+    print("This is my own first program run always when I run directly")
+
+
+def common_program():
+    print("This is common program, everyone can use it")
+
+
+if __name__ == '__main__':
+    main()
+
+
+
---
+++ class-programs/misc/P-150/second_program.py
@@ -0,0 +1,9 @@
+import first_program
+
+
+def main():
+    print("This is my own second program run always when I run directly")
+
+
+if __name__ == '__main__':
+    main()
