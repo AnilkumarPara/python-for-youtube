d2392940ac0fed694a32b9e9257a8a89a1e13da3
Anilkumar Para <50998167+AnilkumarPara@users.noreply.github.com>
2024-02-18 18:03:21 +0530

    Update P40-list-tuple-slicing-assignment-Q-1.py

--- assignment-questions/P40-list-tuple-slicing-assignment-Q-1.py
+++ assignment-questions/P40-list-tuple-slicing-assignment-Q-1.py
@@ -3,8 +3,3 @@ WAP to retrieve 'Python' in reverse order using negative indexing.
 You should use Indexing and slicing Together. Please use below list.
 """
 l1 = [1, 7.3, [33, 22], 'Hello Python']
-
-
-
-
-
