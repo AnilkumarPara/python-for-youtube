c3392f8dbba02c69fe0324a2c08f8c531c255033
Anilkumar Para <50998167+AnilkumarPara@users.noreply.github.com>
2024-02-18 18:00:55 +0530

    Update P34-Collections Assignment-Q-3.py

--- assignment-questions/P34-Collections Assignment-Q-3.py
+++ assignment-questions/P34-Collections Assignment-Q-3.py
@@ -3,3 +3,9 @@ Retrieve 'G' from last_name using following dictionary with negative indexing
 Retrieve 3000 from salaries using following dictionary with positive indexing
 Retrieve 50 from Age using following dictionary with negative indexing
 """
+D1 = {'names': [{'first_name': 'Anilkumar', 'last_name': 'Para'},
+                {'first_name': 'Sachin', 'last_name': 'Tendulkar'},
+                {'first_name': 'Mahesh', 'last_name': 'G'}],
+      'Salaries': [2000, 3000, 4000],
+      'Age': (35, 50, 47)
+      }
